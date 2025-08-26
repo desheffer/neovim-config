52bda4a5e75d7e940cfee6fc87f372e963383935
Doug Sheffer <desheffer@gmail.com>
2025-08-26 13:49:16 -0400

    Update diagnostic signs

--- neovim/lsp.nix
+++ neovim/lsp.nix
@@ -233,10 +233,28 @@ in
         capabilities = capabilities,
       })
 
-      vim.fn.sign_define("DiagnosticSignError", {text = "󰀩", texthl = "DiagnosticSignError"})
-      vim.fn.sign_define("DiagnosticSignWarn",  {text = "󰀦", texthl = "DiagnosticSignWarn"})
-      vim.fn.sign_define("DiagnosticSignInfo",  {text = "󰋼", texthl = "DiagnosticSignInfo"})
-      vim.fn.sign_define("DiagnosticSignHint",  {text = "󰌵", texthl = "DiagnosticSignHint"})
+      vim.diagnostic.config({
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "󰀩",
+            [vim.diagnostic.severity.WARN] = "󰀦",
+            [vim.diagnostic.severity.INFO] = "󰋼",
+            [vim.diagnostic.severity.HINT] = "󰌵",
+          },
+          texthl = {
+            [vim.diagnostic.severity.ERROR] = "Error",
+            [vim.diagnostic.severity.WARN] = "Warn",
+            [vim.diagnostic.severity.HINT] = "Hint",
+            [vim.diagnostic.severity.INFO] = "Info",
+          },
+          numhl = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.HINT] = "",
+            [vim.diagnostic.severity.INFO] = "",
+          },
+        },
+      })
 
       require("CopilotChat").setup({})
     '';
